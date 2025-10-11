3303d230b0813896a0aad9c6f409fc0938aa2224
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2025-10-12 00:49:33 +1300

    Started refactoring logging in
    Going forward I'll have to login quite a bit so I'm extracting the logging in functionality and popping it into a seperate file so it's quicker and easier to maintain.

--- tests/Contact/StaffUserActionsMessage.spec.ts
+++ tests/Contact/StaffUserActionsMessage.spec.ts
@@ -1,19 +1,25 @@
 import { test, expect } from '@playwright/test';
 import { LoginSelectors } from '../Selectors/UserLoginSelectors';
+import {Login} from '../utils/Login'
+
+
 
 test.describe('Staff user actions a message',()=>{
-    test.beforeEach(async({page, isMobile})=>{
-        await page.goto('/');
+    test.beforeEach(async({page, isMobile,})=>{
+    await page.goto('/');
         if (isMobile){
             await page.locator(LoginSelectors.HamburgerIcon).click();
         }
+
     })
 
-    test('Staff user validates new message', async(page)=>{
-
+    test('Staff user validates new message', async({page})=>{
+        const staffLogin = new Login(page)
+        await staffLogin.NavToLoginPage();
+        await staffLogin.Login(process.env.STAFF_USER_LOGIN || '', process.env.CLIENT_PASSWORD || '');
+    });
+        
     })
     test('Staff changes the status of a message',async(page)=>{
         
     })
-
-})
