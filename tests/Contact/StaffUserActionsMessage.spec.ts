9931c441609a42a91163ebea02d7f047a7fd5d6e
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2025-10-18 22:32:54 +1300

    Update StaffUserActionsMessage.spec.ts
    Tried to make the flow less flaky, making the test case more independent and not rely on other prior tests to be successful.

--- tests/Contact/StaffUserActionsMessage.spec.ts
+++ tests/Contact/StaffUserActionsMessage.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 import { LoginSelectors } from '../Selectors/UserLoginSelectors';
 import {Login} from '../utils/Login'
 import { MessageSelectors } from '../Selectors/MessagesSelectors';
+import { ContactSelectors } from '../Selectors/ContactSelectors';
 
 
 
@@ -14,32 +15,32 @@ test.describe('Staff user actions a message',()=>{
 
     })
 
+
     test('Staff user validates new message', async({page})=>{
         const staffLogin = new Login(page)
         await staffLogin.NavToLoginPage();
         await staffLogin.Login(process.env.STAFF_USER_LOGIN || '', process.env.STAFF_PASSWORD || '');
+        await page.locator(ContactSelectors.ContactNavButton).click();
+        await page.locator(ContactSelectors.Subject).selectOption('Payments');
+        await page.locator(ContactSelectors.FormBox).fill("This is a whooooooooooole lot of words. Hopefully 50 words long.")
+        await page.locator(ContactSelectors.SendButton).click();
         await page.locator(MessageSelectors.UserDropdown).click();
         await page.locator(MessageSelectors.MessagesButton).click();
-        await page.locator(MessageSelectors.MessageDetails).click();
+        await page.getByText("Details").first().click();
         await expect(MessageSelectors.AddReplyTextBox).not.toBeUndefined();
-
-
-
-        
     });
         
-    })
     test('Staff changes the status of a message',async({page})=>{
         const staffLogin = new Login(page)
         await staffLogin.NavToLoginPage();
         await staffLogin.Login(process.env.STAFF_USER_LOGIN || '', process.env.STAFF_PASSWORD || '');
-        await page.locator(MessageSelectors.UserDropdown).click();
-        await page.locator(MessageSelectors.MessagesButton).click();
-        await page.locator(MessageSelectors.MessageDetails).click();
+        await page.locator(ContactSelectors.ContactNavButton).click();
+        await page.getByText("Details").first().click();
         await page.locator(MessageSelectors.MessageStatus).selectOption("RESOLVED");
         await page.locator(MessageSelectors.MessageTextField).fill("This has been resolved");
         await page.locator(MessageSelectors.ReplySubmit).click();
         await expect(MessageSelectors.AddReplyTextBox).toContain("This has been resolved")
-        //add assert
     })
+
+})
 
