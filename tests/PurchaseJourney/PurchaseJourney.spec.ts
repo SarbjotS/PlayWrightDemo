363155ef4e4361e5f00bf7dbe89727e17ce6105b
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2025-10-24 22:37:06 +1300

    Create PurchaseJourney.spec.ts

---
+++ tests/PurchaseJourney/PurchaseJourney.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { LoginSelectors } from '../Selectors/UserLoginSelectors';
+import { ContactSelectors } from '../Selectors/ContactSelectors';
+
+
+
+test.describe("Client going through purchase journey",async()=>{
+
+    test.beforeEach(async({page, isMobile,})=>{
+    await page.goto('/');
+        if (isMobile){
+            await page.locator(LoginSelectors.HamburgerIcon).click();
+        }
+
+    })
+    test("Client buys a poer tool",async({page})=>{
+
+    })
+
+    test("Client uses a filter",async({page})=>{
+
+    })
+
+    test("Client purchases a favourited item",async({page})=>{
+
+    })
+    
+    test("Client buys as an admin",async({page})=>{
+
+    })
+
+    
+
+})
